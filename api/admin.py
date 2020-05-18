6f92d8b6682a4f927c77c2e2da8516b361c21d6f
alpden550 <alpden550@gmail.com>
2020-05-18 15:38:59 +0300

    Added user in admin.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from api.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """Custom admin for custom user."""
+    ordering = ('id',)
+    list_display = ('id', 'email', 'name', 'is_staff', 'is_superuser',)
+    search_fields = ('email', 'name')
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password'),
+        }),
+        ('Personal Info', {'fields': ('name',)}),
+        (
+            'Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',)}
+        ),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'name', 'is_staff', 'groups',),
+        }),
+    )

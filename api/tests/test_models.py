e25108ba2b719f430b5572e9918d89a78dfd2d26
alpden550 <alpden550@gmail.com>
2020-05-19 16:44:02 +0300

    Added tests for models.

---
+++ api/tests/test_models.py
@@ -0,0 +1,66 @@
+import pytest
+from django.contrib.auth import get_user_model
+
+from api import models
+
+
+@pytest.mark.django_db
+def test_create_user_successful():
+    """test creating a new user is successful."""
+    email = 'test@gmail.com'
+    password = 'password'
+    user = get_user_model().objects.create_user(
+        email=email,
+        password=password,
+    )
+
+    assert get_user_model().objects.count() == 1
+    assert user.email == email
+    assert user.check_password(password)
+
+
+@pytest.mark.django_db
+def test_user_email_normalized():
+    """Test the email for new user is normalized"""
+    email = 'test@GMAiL.COm'
+    user = get_user_model().objects.create_user(email, 'password')
+
+    assert user.email == email.lower()
+
+
+@pytest.mark.django_db
+def test_create_user_invalid_email():
+    """Test creating user without email raise error."""
+    with pytest.raises(ValueError):
+        get_user_model().objects.create_user(None, 'password')
+
+
+@pytest.mark.django_db
+def test_create_superuser():
+    """Test creating a new superuser"""
+    user = get_user_model().objects.create_superuser('test.gmail.com', 'password')
+
+    assert user.is_staff
+    assert user.is_superuser
+
+
+@pytest.mark.django_db
+def test_movie_str():
+    """Test movie string representation."""
+    movie = models.Movie.objects.create(title='New Movie')
+
+    assert movie.title == str(movie)
+
+
+@pytest.mark.django_db
+def test_rating_str():
+    """Test rating string representation."""
+    user = get_user_model().objects.create_user('test@gmail.com')
+    movie = models.Movie.objects.create(title='New Movie')
+    rating = models.Rating.objects.create(
+        stars=5,
+        movie=movie,
+        user=user,
+    )
+
+    assert str(rating) == f'Rating {rating.pk} for {movie}'
